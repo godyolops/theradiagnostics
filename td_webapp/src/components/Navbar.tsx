9fa9310b46e2d795d83b1ec691b21a5e14c59e63
Gerry Tan Jr <gerrytanjr@gmail.com>
2024-09-04 21:39:29 +0800

    made the navbar responsive so that it wont block the featured buttons for third party apps on mobile view

--- td_webapp/src/components/Navbar.tsx
+++ td_webapp/src/components/Navbar.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="bg-gray-800 p-4">
-      <div className="container mx-auto flex justify-between">
+      <div className="container mx-auto flex justify-between items-center">
         <div className="text-white text-lg font-bold">MyApp</div>
-        <div className="space-x-4">
+        <div className="hidden md:flex space-x-4">
           <Link to="/" className="text-white hover:text-gray-400">
             Home
           </Link>
@@ -14,7 +20,48 @@ const Navbar: React.FC = () => {
             About
           </Link>
         </div>
+        <div className="md:hidden">
+          <button
+            onClick={toggleMenu}
+            className="text-white focus:outline-none"
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              ></path>
+            </svg>
+          </button>
+        </div>
       </div>
+      {isOpen && (
+        <div className="md:hidden">
+          <div className="flex flex-col space-y-2 mt-2">
+            <Link
+              to="/"
+              className="text-white hover:text-gray-400"
+              onClick={toggleMenu}
+            >
+              Home
+            </Link>
+            <Link
+              to="/about"
+              className="text-white hover:text-gray-400"
+              onClick={toggleMenu}
+            >
+              About
+            </Link>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
