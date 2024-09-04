6142612569c34bf8b910dfdc95a4e52d51a988c1
Gerry Tan Jr <gerrytanjr@gmail.com>
2024-09-04 21:35:14 +0800

    add initial codes

---
+++ td_webapp/src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between">
+        <div className="text-white text-lg font-bold">MyApp</div>
+        <div className="space-x-4">
+          <Link to="/" className="text-white hover:text-gray-400">
+            Home
+          </Link>
+          <Link to="/about" className="text-white hover:text-gray-400">
+            About
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
