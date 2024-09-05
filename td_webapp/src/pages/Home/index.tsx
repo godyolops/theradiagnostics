47f3183daa7829059935a994c869ab9d2862f6be
Gerry Tan Jr <gerrytanjr@gmail.com>
2024-09-06 00:02:18 +0800

    test updates

--- td_webapp/src/pages/Home/index.tsx
+++ td_webapp/src/pages/Home/index.tsx
@@ -4,7 +4,7 @@ const Home: React.FC = () => {
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold">Home Page</h1>
-      <p className="mt-2 text-lg">Welcome to the Home Page!</p>
+      <p className="mt-2 text-lg">Welcome to the Home Page</p>
     </div>
   );
 };
