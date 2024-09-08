d038e135d3e25945c927b81dbad2553dbbbd6b9d
Gerry Tan Jr <gerrytanjr@gmail.com>
2024-09-08 23:24:06 +0800

    updated icons and sticky header

--- td_webapp/src/components/Footer.tsx
+++ td_webapp/src/components/Footer.tsx
@@ -1,35 +1,42 @@
 import React from "react";
 
-// images
-import facebookIcon from "../assets/navbar-top/facebook-icon.png";
-import instagramIcon from "../assets/navbar-top/instagram-icon.png";
-import linkedinIcon from "../assets/navbar-top/linkedin-icon.png";
-import twitterIcon from "../assets/navbar-top/twitter-icon.png";
+import {
+  faFacebookF,
+  faInstagram,
+  faLinkedinIn,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Footer: React.FC = () => {
-    return (
-        <footer className="bg-gray-900 max-h-64 h-20 place-content-center z-50">
-            <div className="container mx-auto max-w-6xl px-3 flex justify-between">
-                <div className="text-white place-content-center">
-                    Copyright ©2024 All rights reserved | This template is made with  by Colorlib
-                </div>
-                <div className="flex">
-                    <div className="p-2.5 bg-white mr-4 mt-1.5 b-2 hover:bg-gradient-to-r from-blue-500 to-green-500">
-                     <img src={facebookIcon} alt="" height={1} width={12} className="hover:fill-white"/>
-                    </div>
-                    <div className="p-2.5 bg-white mr-4 mt-1.5 b-2 hover:bg-gradient-to-r from-blue-500 to-green-500">
-                        <img src={instagramIcon} alt="" height={1} width={12} className=""/>
-                    </div>
-                    <div className="p-2.5  bg-white mr-4 mt-1.5 b-2 hover:bg-gradient-to-r from-blue-500 to-green-500">
-                        <img src={linkedinIcon} alt="" height={1} width={12} className=""/>
-                    </div>
-                    <div className="p-2.5 bg-white mr-4 mt-1.5 b-2 hover:bg-gradient-to-r from-blue-500 to-green-500">
-                        <img src={twitterIcon} alt="" height={1} width={12} className=""/>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    );
-}
+  return (
+    <footer className="bg-gray-900 max-h-64 h-20 place-content-center z-50">
+      <div className="container mx-auto max-w-6xl px-3 flex justify-between">
+        <div className="text-white place-content-center">
+          Copyright ©2024 All rights reserved | This template is made with by
+          Colorlib
+        </div>
+        <div className="flex gap-x-3">
+          <FontAwesomeIcon
+            icon={faFacebookF}
+            className="text-slate-500 cursor-pointer p-2.5 bg-white hover:bg-gradient-to-r from-blue-500 to-green-500"
+          />
+          <FontAwesomeIcon
+            icon={faInstagram}
+            className="text-slate-500 cursor-pointer p-2.5  bg-white hover:bg-gradient-to-r from-blue-500 to-green-500"
+          />
+          <FontAwesomeIcon
+            icon={faLinkedinIn}
+            className="text-slate-500 cursor-pointer p-2.5  bg-white hover:bg-gradient-to-r from-blue-500 to-green-500"
+          />
+          <FontAwesomeIcon
+            icon={faTwitter}
+            className="text-slate-500 cursor-pointer p-2.5  bg-white hover:bg-gradient-to-r from-blue-500 to-green-500"
+          />
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
--- td_webapp/src/components/HeaderTop.tsx
+++ td_webapp/src/components/HeaderTop.tsx
@@ -1,37 +1,79 @@
 import React from "react";
 
-// images
-import facebookIcon from "../assets/navbar-top/facebook-icon.png";
-import instagramIcon from "../assets/navbar-top/instagram-icon.png";
-import linkedinIcon from "../assets/navbar-top/linkedin-icon.png";
-import twitterIcon from "../assets/navbar-top/twitter-icon.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faInstagram,
+  faLinkedinIn,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons"; // Outlined version
+import {
+  faLocationDot,
+  faMobileScreenButton,
+} from "@fortawesome/free-solid-svg-icons";
 
 const HeaderTop: React.FC = () => {
-    return (
-        <div className="z-1 top-0 left-0 py-3.5 bg-white w-full">
-            <div className="container mx-auto max-w-6xl px-3">
-            <div className="flex flex-wrap justify-between text-center">
-                    <div className="flex">
-                        <div className="mr-4 text-slate-500 text-xs font-sans font-medium mt-1.5 b-2">
-                            CALL US NOW! +639123456789
-                        </div>
-                        <div className="mr-4 text-slate-500 text-xs font-sans font-medium mt-1.5 b-2">
-                            MEDICAL@EXAMPLE.COM
-                        </div>
-                        <div className="text-slate-500 text-xs font-sans font-medium mt-1.5 b-2">
-                            FIND OUT LOCATION
-                        </div>
-                    </div>
-                    <div className="flex">
-                        <img src={facebookIcon} alt="" height={2} width={12} className="mr-4 mt-1.5 b-2 grayscale-50"/>
-                        <img src={instagramIcon} alt="" height={1} width={12} className="mr-4 mt-1.5 b-2"/>
-                        <img src={linkedinIcon} alt="" height={1} width={12} className="mr-4 mt-1.5 b-2"/>
-                        <img src={twitterIcon} alt="" height={1} width={12} className="mt-1.5 b-2"/>
-                    </div>
-                </div>
+  return (
+    <div className="z-1 top-0 left-0 py-3.5 bg-white w-full">
+      <div className="container mx-auto max-w-6xl px-3">
+        <div className="flex flex-wrap justify-between text-center">
+          <div className="flex gap-x-3">
+            <div className="flex justify-center items-center gap-x-2 text-slate-500">
+              <div>
+                <FontAwesomeIcon icon={faMobileScreenButton} />
+              </div>
+              <div className="text-xs font-sans font-medium">
+                CALL US NOW! +639123456789
+              </div>
             </div>
+            <div className="flex justify-center items-center gap-x-2 text-slate-500">
+              <div>
+                <FontAwesomeIcon icon={faEnvelope} />
+              </div>
+              <div className="text-xs font-sans font-medium">
+                theradiagnostics@gmail.com
+              </div>
+            </div>
+            <div className="flex justify-center items-center gap-x-2 text-slate-500">
+              <div>
+                <FontAwesomeIcon icon={faLocationDot} />
+              </div>
+              <div className="text-xs font-sans font-medium">
+                FIND OUR LOCATION
+              </div>
+            </div>
+          </div>
+          <div className="flex gap-x-3">
+            <div>
+              <FontAwesomeIcon
+                icon={faFacebookF}
+                className="text-slate-500 cursor-pointer"
+              />
+            </div>
+            <div>
+              <FontAwesomeIcon
+                icon={faInstagram}
+                className="text-slate-500 cursor-pointer"
+              />
+            </div>
+            <div>
+              <FontAwesomeIcon
+                icon={faLinkedinIn}
+                className="text-slate-500 cursor-pointer"
+              />
+            </div>
+            <div>
+              <FontAwesomeIcon
+                icon={faTwitter}
+                className="text-slate-500 cursor-pointer"
+              />
+            </div>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default HeaderTop;
